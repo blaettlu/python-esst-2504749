dc8349146967814329317e0355faabb780e9b4f1
blaettlu <blaettlu1@bluewin.ch>
2025-06-26 07:04:58 +0000

    Änderungen 4

--- uebungs_datei.py
+++ uebungs_datei.py
@@ -1,7 +1,21 @@
 # Das ist ein Kommentar in Ihrer Uebungsdatei
 
 
-var1 = 15
-var2 = 4
-print("Die Multiplikation von " + str(var1) + " mit " + str(var2) + " ist: ",var1*var2)
-print("Die Summe von " + str(var1) + " mit " + str(var2) + " ist: ",var1+var2)
+my_list = [0,10,100]
+
+def ausgabe_meine_liste(paramliste):
+  for zahl in paramliste:
+    print('Das aktuelle Element ist: ',zahl)
+
+ausgabe_meine_liste(my_list)
+
+gkzahl1 = 3.1
+gkzahl2 = 2.5
+
+def meine_multiplikation(zahl1, zahl2):
+  print('Die Multiplikation von ' + str(zahl1) + ' mit ' + str(zahl2+2) + ' ist: ',zahl1 * (zahl2+2))
+
+meine_multiplikation(gkzahl1,gkzahl2)
+
+
+
