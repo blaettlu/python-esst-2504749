f478feea0db24eba2fbcb0f414e77042733e4532
blaettlu <blaettlu1@bluewin.ch>
2025-06-27 06:22:31 +0000

    Änderung 5

--- uebungs_datei.py
+++ uebungs_datei.py
@@ -1,21 +1,14 @@
 # Das ist ein Kommentar in Ihrer Uebungsdatei
 
 
-my_list = [0,10,100]
-
-def ausgabe_meine_liste(paramliste):
-  for zahl in paramliste:
-    print('Das aktuelle Element ist: ',zahl)
-
-ausgabe_meine_liste(my_list)
-
 gkzahl1 = 3.1
 gkzahl2 = 2.5
 
 def meine_multiplikation(zahl1, zahl2):
-  print('Die Multiplikation von ' + str(zahl1) + ' mit ' + str(zahl2+2) + ' ist: ',zahl1 * (zahl2+2))
+  multipl = zahl1 * (zahl2+2)
+  return multipl
 
-meine_multiplikation(gkzahl1,gkzahl2)
+print('Das Ergebnis ist: ',meine_multiplikation(gkzahl1,gkzahl2))
 
 
 
