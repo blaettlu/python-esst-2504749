9483497520de0179b17b2bfc149eacad32abcfbc
blaettlu <blaettlu1@bluewin.ch>
2025-07-14 09:35:47 +0000

    Änderungen 12

--- uebungs_datei.py
+++ uebungs_datei.py
@@ -1,3 +1,5 @@
+# TicTakToe mit Zufallsgenerator
+
 
 
 import random
@@ -11,23 +13,63 @@ sp2 = 'o'
 spielbrett = [le,le,le,le,le,le,le,le,le]
 
 def ausgabe_spielbrett():
-   print("Spielbrett:")
-   brett = spielbrett
-   print(brett[0],brett[1],brett[2])
-   print(brett[3],brett[4],brett[5])
-   print(brett[6],brett[7],brett[8])
+	print("Spielbrett:")
+	brett = spielbrett
+	print(brett[0],brett[1],brett[2])
+	print(brett[3],brett[4],brett[5])
+	print(brett[6],brett[7],brett[8])
   
+def stein_setzen_random(sp):
+	kr = 0
+	while kr == 0:
+		p = random.randint(0,8)
+		if spielbrett[p] == le:
+			spielbrett[p] = sp
+			kr = 1
+
 def stein_setzen(sp):
-   kr = 0
-   while kr == 0:
-      p = random.randint(0,8)
-      if spielbrett[p] == le:
-         spielbrett[p] = sp
-         kr = 1
+	user_eingabe = 0
+	while user_eingabe < 1 and user_eingabe > 9:
+		user_eingabe = input('Position (1..9) für ' + str(sp) + ' : ')
+    spielbrett[user_eingabe -1] = sp
+ 
+def check_gewinner(sp):
+	brett = spielbrett
+	if brett[0] == sp and brett[1] == sp and brett[2] == sp:
+		return True
+	if brett[3] == sp and brett[4] == sp and brett[5] == sp:
+		return True
+	if brett[6] == sp and brett[7] == sp and brett[8] == sp:
+		return True
+	if brett[0] == sp and brett[3] == sp and brett[6] == sp:
+		return True
+	if brett[1] == sp and brett[4] == sp and brett[7] == sp:
+		return True
+	if brett[2] == sp and brett[5] == sp and brett[8] == sp:
+		return True
+	if brett[0] == sp and brett[4] == sp and brett[8] == sp:
+		return True
+	if brett[6] == sp and brett[4] == sp and brett[2] == sp:
+		return True
+	else:
+		return False
+	
 
 ausgabe_spielbrett()
+wi = 0
 for i in range(4):
-   stein_setzen(sp1)
-   stein_setzen(sp2)
-ausgabe_spielbrett()
-
+	stein_setzen(sp1)
+	if check_gewinner(sp1) == True:
+		ausgabe_spielbrett()
+		print("Spieler " + sp1 + " hat gewonnen")
+		wi = 1
+		break
+	stein_setzen(sp2)
+	if check_gewinner(sp2) == True:
+		ausgabe_spielbrett()
+		print("Spieler " + sp2 + " hat gewonnen")
+		wi = 1
+		break
+	ausgabe_spielbrett()
+if wi == 0:
+	print("Unentschieden")
